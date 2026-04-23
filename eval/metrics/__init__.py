7bf487414be6b3e99130e84b3976a41e2c9136cd
Ash <aashen1@foxmail.com>
2026-04-24 02:39:53 +0800

    feat: add calculate_retrieval_diversity metric
    Measures the ratio of unique documents to total results in top-k.
    Value of 1.0 means all results from different documents; close to 0
    means all from the same document. Registered in BuiltinEvaluator.

--- eval/metrics/__init__.py
+++ eval/metrics/__init__.py
@@ -16,6 +16,7 @@ from eval.metrics.generation import (
     _verify_statements,
     calculate_answer_relevancy,
     calculate_faithfulness,
+    calculate_hallucination_rate,
 )
 from eval.metrics.llm_retrieval import (
     _can_infer_from_context,
@@ -24,7 +25,12 @@ from eval.metrics.llm_retrieval import (
     calculate_context_precision,
     calculate_context_recall,
 )
-from eval.metrics.retrieval import calculate_hit_rate, calculate_mrr, calculate_ndcg
+from eval.metrics.retrieval import (
+    calculate_hit_rate,
+    calculate_mrr,
+    calculate_ndcg,
+    calculate_retrieval_diversity,
+)
 from eval.metrics.utils import (
     _create_llm_client,
     _parse_chunk_id,
@@ -33,11 +39,11 @@ from eval.metrics.utils import (
 )
 
 __all__ = [
-    "calculate_hit_rate", "calculate_mrr", "calculate_ndcg",
+    "calculate_hit_rate", "calculate_mrr", "calculate_ndcg", "calculate_retrieval_diversity",
     "calculate_chunk_hit_rate", "calculate_chunk_mrr", "calculate_chunk_ndcg",
     "deduplicate_by_document", "calculate_dedup_hit_rate", "calculate_dedup_mrr", "calculate_dedup_ndcg",
     "calculate_false_positive_rate",
-    "calculate_faithfulness", "calculate_answer_relevancy",
+    "calculate_faithfulness", "calculate_answer_relevancy", "calculate_hallucination_rate",
     "calculate_context_precision", "calculate_context_recall",
     "normalize_source", "normalize_source_with_equivalence",
 ]
