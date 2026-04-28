3b1d0c855d2e3a81996cb8836bf0dff6ad56f1ce
Ash <aashen1@foxmail.com>
2026-04-29 04:54:19 +0800

    style: remove unused is_genuine_proper_noun import after rebase

--- src/test_generation/generator.py
+++ src/test_generation/generator.py
@@ -49,7 +49,6 @@ from src.test_generation.validators import (
     build_primary_pool,
     calculate_hybrid_quality_metrics,
     detect_content_overlaps,
-    is_genuine_proper_noun,
     validate_answer_consistency,
     validate_answer_evidence_consistency,
     validate_evidence,
